49fa04858108e973b56c134a7540ce4108833eac
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-22 22:53:59 +0530

    signIn updated

--- backend/controllers/signIn.Controller.js
+++ backend/controllers/signIn.Controller.js
@@ -1,29 +1,34 @@
-const {User}=require("../models/userSchema")
-exports.signIn=async (req,res)=>{
-    const{username,firstName}=req.body;
+const { User } = require("../models/userSchema");
 
-    try {
-        const userExist=await User.find({
-            username:username
-        })
-        console.log(userExist,"???user exist");
-        if(!userExist){
-            return res.status(404).json({
-                success:false,
-                message:"User not exist,Please SignIn"
-            })
-        }
-        return res.status(200).json({
-            success:true,
-            message:"User Signed successfullt"
-        })
-        
-    } catch (error) {
-        console.log(error,"Error while signin");
-        return res.status(500).json({
-            success:false,
-            message:error
-        })
+exports.signIn = async (req, res) => {
+  const { username, firstName } = req.body;
+
+  try {
+    const userExist = await User.findOne({ username: username });
+
+    console.log(userExist, "???user exist");
+
+    if (!userExist) {
+      return res.status(404).json({
+        success: false,
+        message: "User does not exist. Please sign up.",
+      });
     }
 
-}
+    return res.status(200).json({
+      success: true,
+      message: "User signed in successfully",
+      data: {
+        username: userExist.username,
+        userId: userExist._id,
+        // Other data you want to include
+      },
+    });
+  } catch (error) {
+    console.log(error, "Error while signing in");
+    return res.status(500).json({
+      success: false,
+      message: "Error while signing in",
+    });
+  }
+};
