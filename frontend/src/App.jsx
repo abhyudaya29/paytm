264ac20b98b3b4aaa2731c5727303493330a17ad
abhyudaya29 <abhyudayadev123@gmail.com>
2024-04-22 13:43:32 +0530

    deployment2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div>
         
         <Routes>
-          {/* <Route path='/' element={<Home/>} /> */}
+          <Route path='/' element={<Home/>} />
           <Route path='/signup'element={<SignUp/>}/>
           <Route path='/signin'element={<SignIn/>}/>
           <Route path='/dashboard'element={<Dashboard/>}/>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import Button from './Button'
 
 const Home = () => {
+  const navigate=useNavigate()
   return (
-    <div>Home</div>
+    <>
+    <div>
+      <h2>Welcome To Patm app</h2>
+      <Link to={'/signup'}>
+      <Button label={"Click for signup"}>
+
+      </Button>
+      </Link>
+    </div>
+    </>
   )
 }
 
